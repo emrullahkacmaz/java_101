b99f9054174be405f354602d82f8c4a23ad83e7b
emrullahkacmaz <91068822+emrullahkacmaz@users.noreply.github.com>
2022-03-03 12:01:30 +0300

    java_Hello_World

---
+++ javapatikasi/src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("artık başlıyoruz \n kardeşim");
+        char vChar = 'a';
+        String kelimeler ="hello world";
+        System.out.println(kelimeler);
+        int a = 50;
+        int b= 24;
+        int c= a %b ;
+        System.out.println(c);
+
+
+    }
+}
